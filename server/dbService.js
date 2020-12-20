f9d8b2143afa795a7b2b725ec08c1fef968749b5
j9972 <j9972@naver.com>
2020-12-20 14:29:37 +0900

    정리

--- server/dbService.js
+++ server/dbService.js
@@ -2,26 +2,26 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connection = mysql.createConnection({
-    host : process.env.HOST,
-    user : process.env.USER,
-    password : process.env.PASSWORD,
-    database : process.env.DATABASE,
-    port : process.env.DB_PORT,
-});
+// const connection = mysql.createConnection({
+//     host : process.env.HOST,
+//     user : process.env.USER,
+//     password : process.env.PASSWORD,
+//     database : process.env.DATABASE,
+//     port : process.env.DB_PORT,
+// });
 
-// const connection = mysql.createConnection ({
-//     host : "localhost",
-//     user : "tutorial",
-//     password : "password",
-//     database : 'web_app',
-//     port : 3306,
-//     insecureAuth : true,
-// })
+const connection = mysql.createConnection ({
+    host : "localhost",
+    user : "tutorial",
+    password : "password",
+    database : 'web_app',
+    port : 3306,
+    insecureAuth : true,
+})
 
 connection.connect((err) => {
     if(err) {
-        console.log(err.message + '에러남');
+        console.log(err.message);
     }
     console.log('db' + ' ' + connection.state);
 })
